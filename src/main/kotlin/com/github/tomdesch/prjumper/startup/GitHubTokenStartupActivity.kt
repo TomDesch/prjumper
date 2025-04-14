2c3bc0250940528635dd1d8d0f2fc716512b9a7f
Tom Descheemaeker <tom.descheemaeker@iqgeo.com>
2025-04-14 14:07:05 +0200

    enhance GitHubTokenStartupActivity and plugin.xml to register token startup activity

--- src/main/kotlin/com/github/tomdesch/prjumper/startup/GitHubTokenStartupActivity.kt
+++ src/main/kotlin/com/github/tomdesch/prjumper/startup/GitHubTokenStartupActivity.kt
@@ -2,7 +2,6 @@ package com.github.tomdesch.prjumper.startup
 
 import com.github.tomdesch.prjumper.auth.GitHubTokenService
 import com.github.tomdesch.prjumper.ui.GitHubTokenDialog
-import com.intellij.openapi.components.Service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.ProjectActivity
 import kotlinx.coroutines.Dispatchers
@@ -11,7 +10,6 @@ import okhttp3.OkHttpClient
 import okhttp3.Request
 import java.awt.EventQueue
 
-@Service(Service.Level.PROJECT)
 class GitHubTokenStartupActivity : ProjectActivity {
     override suspend fun execute(project: Project) {
         val storedToken = GitHubTokenService.getInstance().getToken()
